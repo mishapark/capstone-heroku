251bbf09a8c2d2578f581f4e837dc05f222ff639
Tim Baklanov <timbaklanov03@gmail.com>
2022-03-24 16:30:14 -0400

    Changed the color of analytics

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -9,112 +9,106 @@ import { Button, Card, Container } from "@material-ui/core";
 import { Stack } from "@mui/material";
 import DropdownList from "react-widgets/DropdownList";
 
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
-import TabContext from '@material-ui/lab/TabContext';
-import TabList from '@material-ui/lab/TabList';
-import TabPanel from '@material-ui/lab/TabPanel';
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import TabContext from "@material-ui/lab/TabContext";
+import TabList from "@material-ui/lab/TabList";
+import TabPanel from "@material-ui/lab/TabPanel";
 
 import { getStandards } from "../api/standards";
 
-import ProductsByStandard from '../components/AnalyticsData/ProductsByStandard';
-import ProductsByCompliance from '../components/AnalyticsData/ProductsByCompliance';
-import RfqOverviewByStatus from '../components/AnalyticsData/RfqOverviewByStatus';
+import ProductsByStandard from "../components/AnalyticsData/ProductsByStandard";
+import ProductsByCompliance from "../components/AnalyticsData/ProductsByCompliance";
+import RfqOverviewByStatus from "../components/AnalyticsData/RfqOverviewByStatus";
 
 function Analytics() {
-    const [products, setProducts] = useState([]);
-    const [rfqs, setRfqs] = useState([]);
-    const [analyticsTabValue, setAnalyticsTabValue] = useState("1");
+  const [products, setProducts] = useState([]);
+  const [rfqs, setRfqs] = useState([]);
+  const [analyticsTabValue, setAnalyticsTabValue] = useState("1");
 
-    const [standards, setStandards] = useState([]);
+  const [standards, setStandards] = useState([]);
 
-    const sendGetRequest = async () => {
-        try {
-            const response = await axios.get(
-                "https://humber-capstone-backend.herokuapp.com/products"
-            );
-            setProducts(response.data);
-            const response2 = await axios.get(
-                "https://humber-capstone-backend.herokuapp.com/rfqs"
-            );
-            setRfqs(response2.data);
-        } catch (err) {
-            console.log(err.message);
-        }
-    };
-
-    useEffect(() => {
-        sendGetRequest();
-        getStandards().then((data) => setStandards(data));
-    }, []);
-
-    
-
-    const handleAnalyticsTabChange = (event, value) => {
-        setAnalyticsTabValue(value)
-
-        switch (value) {
-            case '1':
-                return 'bar';
-
-            case '2':
-                return 'bar';
-            default:
-                return 'foo';
-        }
-
+  const sendGetRequest = async () => {
+    try {
+      const response = await axios.get(
+        "https://humber-capstone-backend.herokuapp.com/products"
+      );
+      setProducts(response.data);
+      const response2 = await axios.get(
+        "https://humber-capstone-backend.herokuapp.com/rfqs"
+      );
+      setRfqs(response2.data);
+    } catch (err) {
+      console.log(err.message);
     }
+  };
 
-    return (
-        <Stack spacing={2}>
-            <Card
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    padding: 2,
-                    marginBottom: 2,
-                }}
-            >
-                <Typography variant="h5" sx={{ padding: 2 }}>Analytics</Typography>
+  useEffect(() => {
+    sendGetRequest();
+    getStandards().then((data) => setStandards(data));
+  }, []);
 
+  const handleAnalyticsTabChange = (event, value) => {
+    setAnalyticsTabValue(value);
 
+    switch (value) {
+      case "1":
+        return "bar";
 
-            </Card>
+      case "2":
+        return "bar";
+      default:
+        return "foo";
+    }
+  };
 
-            <Card sx={{overflow: "visible"}}>
-                <TabContext value={analyticsTabValue}>
-                    <Tabs
-                        value={analyticsTabValue}
-                        onChange={handleAnalyticsTabChange}
-                        textColor="secondary"
-                        indicatorColor="secondary"
-                        aria-label="secondary tabs example"
-                        centered
-                    >
-                        <Tab value="1" label="Products by Standard" />
-                        <Tab value="2" label="Products by Compliance" />
-                        <Tab value="3" label="RFQ Overview by Status" />
-                    </Tabs>
+  return (
+    <Stack spacing={2}>
+      <Card
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 2,
+          marginBottom: 2,
+        }}
+      >
+        <Typography variant="h5" sx={{ padding: 2 }}>
+          Analytics
+        </Typography>
+      </Card>
 
-                    <TabPanel value="1">
-                        {/*<Typography variant="h6" sx={{ padding: 2 }}>Products by Standard</Typography>*/}
-                        <ProductsByStandard data={standards} />                        
-                    </TabPanel>
+      <Card sx={{ overflow: "visible" }}>
+        <TabContext value={analyticsTabValue}>
+          <Tabs
+            value={analyticsTabValue}
+            onChange={handleAnalyticsTabChange}
+            textColor="secondprimaryary"
+            indicatorColor="primary"
+            aria-label="secondary tabs example"
+            centered
+          >
+            <Tab value="1" label="Products by Standard" />
+            <Tab value="2" label="Products by Compliance" />
+            <Tab value="3" label="RFQ Overview by Status" />
+          </Tabs>
 
-                    <TabPanel value="2">
-                        {/*<Typography variant="h6" sx={{ padding: 2 }}>Products by Compliance</Typography>*/}
-                        <ProductsByCompliance />  
-                    </TabPanel>
-                    
-                    <TabPanel value="3">
-                        {/*<Typography variant="h6" sx={{ padding: 2 }}>RFQ Overview by Status</Typography>*/}
-                        <RfqOverviewByStatus/>
-                    </TabPanel>
+          <TabPanel value="1">
+            {/*<Typography variant="h6" sx={{ padding: 2 }}>Products by Standard</Typography>*/}
+            <ProductsByStandard data={standards} />
+          </TabPanel>
 
-                </TabContext>
+          <TabPanel value="2">
+            {/*<Typography variant="h6" sx={{ padding: 2 }}>Products by Compliance</Typography>*/}
+            <ProductsByCompliance />
+          </TabPanel>
 
-            </Card>
-            {/*
+          <TabPanel value="3">
+            {/*<Typography variant="h6" sx={{ padding: 2 }}>RFQ Overview by Status</Typography>*/}
+            <RfqOverviewByStatus />
+          </TabPanel>
+        </TabContext>
+      </Card>
+      {/*
             <Grid container>
                 <Grid item xs={12} md={6}>
                     <Card
@@ -166,10 +160,8 @@ function Analytics() {
             </Grid>
 
             */}
-
-
-        </Stack>
-    );
+    </Stack>
+  );
 }
 
 export default Analytics;
