020982748bbeed953ff299dafd71fa4a15163848
caebeo <caebeo@hotmail.com>
2022-04-06 22:29:08 -0400

    ce - updated company settings > company

---
+++ src/api/companies.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export const getCompany = (id) => {
+    try {
+        const promise = axios.get(
+            "https://humber-capstone-backend.herokuapp.com/companies/" + id
+        );
+        const data = promise.then((response) => response.data);
+        return data;
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+
+export const updateCompany = (id, company) => {
+    try {
+
+        const promise = axios.put(
+            "https://humber-capstone-backend.herokuapp.com/companies/update/" + id,
+            company
+
+        );
+        const data = promise.then((response) => response.data);
+        return data;
+
+    } catch (err) {
+        console.log(err.message);
+    }
+}
--- src/pages/SettingsTabs/Company.js
+++ src/pages/SettingsTabs/Company.js
@@ -1,84 +1,150 @@
 import { Box, Paper, Toolbar, Typography, Button } from "@mui/material";
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { PageHeader } from "../../components/Header/PageHeader";
 import ApartmentIcon from "@mui/icons-material/Apartment";
 import { Divider, Grid, TextField } from "@material-ui/core";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom"
+
+// api
+import { getCompany, updateCompany } from "../../api/companies";
 
 function Company() {
-  return (
-    <div>
-      <Box sx={{ width: "100%" }}>
-        <Paper sx={{ width: "100%" }}>
-          <PageHeader
-            icon={<ApartmentIcon />}
-            title="Company Settings"
-            description="Current Company Settings"
-          ></PageHeader>
-          <Divider />
-          <br />
-          <Grid container spacing={2} style={{ margin: "1em" }}>
-            <Grid item xs={3}>
-              <Typography>Company Name</Typography>
-            </Grid>
-            <Grid item xs={7}>
-              <TextField
-                variant="outlined"
-                label="Company Name"
-                fullWidth
-              ></TextField>
-            </Grid>
-            <Grid item xs={11}>
-              <Divider variant="middle"></Divider>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography>Company Address</Typography>
-            </Grid>
-            <Grid item xs={7}>
-              <TextField
-                variant="outlined"
-                label="Company Infromation"
-                multiline
-                rows={4}
-                fullWidth
-              ></TextField>
-            </Grid>
-            <Grid item xs={11}>
-              <Divider variant="middle"></Divider>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography>Company Email</Typography>
-            </Grid>
 
-            <Grid item xs={7}>
-              <TextField variant="outlined" label="Email" fullWidth></TextField>
-            </Grid>
-            <Grid item xs={11}>
-              <Divider variant="middle"></Divider>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography>Company Telephone</Typography>
-            </Grid>
+    const navigate = useNavigate();
 
-            <Grid item xs={7}>
-              <TextField
-                variant="outlined"
-                label="Telephone"
-                fullWidth
-              ></TextField>
-            </Grid>
-            <Grid container spacing={2}>
-              <Grid item xs={8} md={6}>
-                <Button type="submit">Save</Button>
-              </Grid>
-              <Grid item xs={8} md={6}>
-                <Button>Cancel</Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Box>
-    </div>
-  );
+    const goBack = () => navigate(-1);
+
+    const { auth } = useAuth();
+    const [company, setCompany] = useState({})
+
+    const [name, setName] = useState("")
+    const [address, setAddress] = useState("")
+    const [email, setEmail] = useState("")
+    const [telephone, setTelephone] = useState("")
+
+    useEffect(() => {
+        // get company from user
+        console.log(auth.companyId)
+        getCompany(auth.companyId)
+            .then((data) => {
+                setCompany(data)
+                setName(data.company_name)
+                setAddress(data.company_address)
+                setEmail(data.company_email)
+                setTelephone(data.company_phone)
+            })
+    }, []);
+
+    const handleSave = (e) => {
+        e.preventDefault()
+
+        let updatedCompany = company
+
+        updatedCompany.company_name = name;
+        updatedCompany.company_address = address;
+        updatedCompany.company_email = email;
+        updatedCompany.company_phone = telephone;
+
+        updateCompany(updatedCompany._id, updatedCompany)
+            .then((data) => {
+                console.log(data)
+            })
+
+    }
+
+    const handleNameChange = (e, field) => {
+        console.log(e.target.value)
+        if (field === "name") {
+            setName(e.target.value)
+        } else if (field === "address") {
+            setAddress(e.target.value)
+        } else if (field === "email") {
+            setEmail(e.target.value)
+        } else if (field === "telephone") {
+            setTelephone(e.target.value)
+        }
+    }
+
+    return (
+        <div>
+            <Box sx={{ width: "100%" }}>
+                <Paper sx={{ width: "100%" }}>
+                    <PageHeader
+                        icon={<ApartmentIcon />}
+                        title="Company Settings"
+                        description="Current Company Settings"
+                    ></PageHeader>
+                    <Divider />
+                    <br />
+                    <Grid container spacing={2} style={{ margin: "1em" }}>
+                        <Grid item xs={3}>
+                            <Typography>Company Name</Typography>
+                        </Grid>
+                        <Grid item xs={7}>
+                            <TextField
+                                variant="outlined"
+                                fullWidth
+                                value={name}
+                                onChange={(e) => handleNameChange(e, "name")}
+                            ></TextField>
+                        </Grid>
+                        <Grid item xs={11}>
+                            <Divider variant="middle"></Divider>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography>Company Address</Typography>
+                        </Grid>
+                        <Grid item xs={7}>
+                            <TextField
+                                variant="outlined"
+                                multiline
+                                rows={4}
+                                fullWidth
+                                value={address}
+                            ></TextField>
+                        </Grid>
+                        <Grid item xs={11}>
+                            <Divider variant="middle"></Divider>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography>Company Email</Typography>
+                        </Grid>
+
+                        <Grid item xs={7}>
+                            <TextField
+                                variant="outlined"
+                                fullWidth
+                                value={email}
+                                ></TextField>
+                        </Grid>
+                        <Grid item xs={11}>
+                            <Divider variant="middle"></Divider>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography>Company Telephone</Typography>
+                        </Grid>
+
+                        <Grid item xs={7}>
+                            <TextField
+                                variant="outlined"
+                                fullWidth
+                                value={telephone}
+                            ></TextField>
+                        </Grid>
+                        <Grid container spacing={2}>
+                            <Grid item xs={8} md={6}>
+                                <Button type="submit" onClick={handleSave}>Save</Button>
+                            </Grid>
+                            <Grid item xs={8} md={6}>
+                                <Button onClick={goBack}> Cancel</Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Box>
+        </div>
+    );
 }
 
 export default Company;
