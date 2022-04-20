16eb285b4ae5f185b3bc90336a6a46130a266641
mishapark <66741910+mishapark@users.noreply.github.com>
2022-04-20 09:49:53 -0400

    added dark theme switch

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import {
 import makeStyles from "@mui/styles/makeStyles";
 import { CssBaseline } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import ColorModeContext from "./context/ColorModeContext";
 
 const messages = {
   en: English,
@@ -44,21 +45,39 @@ const ROLES = {
 
 function App() {
   const [user, setUser] = useState(null);
+  const [mode, setMode] = React.useState("dark");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
 
   const value = useMemo(() => ({ user, setUser }), [user, setUser]);
   const [language, setLanguage] = React.useState("en");
 
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
-  const darkTheme2 = React.useMemo(
-    () =>
-      createTheme(
-        adaptV4Theme({
-          typography: {
-            fontFamily: ["Poppins", "sans-serif"].join(","),
-          },
-          palette: {
-            mode: prefersDarkMode ? "dark" : "light",
+  const getDesignTokens = (mode) => ({
+    palette: {
+      mode,
+      ...(mode === "light"
+        ? {
+            // palette values for light mode
+            primary: {
+              main: "#0024FF",
+            },
+            secondary: {
+              main: "#101637",
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }
+        : {
+            // palette values for dark mode
             primary: {
               main: "#0024FF",
             },
@@ -68,36 +87,64 @@ function App() {
             background: {
               default: "#2e2e2e",
             },
-          },
-          ".MuiButtonBase-root": {
-            color: "#fcfcfc",
-          },
-        })
-      ),
-    [prefersDarkMode]
-  );
+          }),
+    },
+    ".MuiButtonBase-root": {
+      color: "#fcfcfc",
+    },
+    typography: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+    },
+  });
 
-  const theme = createTheme(
-    adaptV4Theme({
-      typography: {
-        fontFamily: ["Poppins", "sans-serif"].join(","),
-      },
-      palette: {
-        primary: {
-          main: "#0024FF",
-        },
-        secondary: {
-          main: "#101637",
-        },
-        background: {
-          default: "#fcfcfc",
-        },
-      },
-      ".MuiButtonBase-root": {
-        color: "#fcfcfc",
-      },
-    })
-  );
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  // const darkTheme2 = React.useMemo(
+  //   () =>
+  //     createTheme(
+  //       adaptV4Theme({
+  //         typography: {
+  //           fontFamily: ["Poppins", "sans-serif"].join(","),
+  //         },
+  //         palette: {
+  //           mode: prefersDarkMode ? "dark" : "light",
+  //           primary: {
+  //             main: "#0024FF",
+  //           },
+  //           secondary: {
+  //             main: "#101637",
+  //           },
+  //           background: {
+  //             default: "#2e2e2e",
+  //           },
+  //         },
+
+  //       })
+  //     ),
+  //   [prefersDarkMode]
+  // );
+
+  // const theme = createTheme(
+  //   adaptV4Theme({
+  //     typography: {
+  //       fontFamily: ["Poppins", "sans-serif"].join(","),
+  //     },
+  //     palette: {
+  //       primary: {
+  //         main: "#0024FF",
+  //       },
+  //       secondary: {
+  //         main: "#101637",
+  //       },
+  //       background: {
+  //         default: "#fcfcfc",
+  //       },
+  //     },
+  //     ".MuiButtonBase-root": {
+  //       color: "#fcfcfc",
+  //     },
+  //   })
+  // );
 
   return (
     <LanguageProvider.Provider
@@ -105,40 +152,42 @@ function App() {
     >
       <IntlProvider locale={language} messages={messages[language]}>
         <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                {/* public routes */}
-                <Route path="/" element={<Home />} />
-                <Route path="unathorized" element={<Unauthorized />} />
-                <Route path="signin" element={<SignIn />} />
-                <Route path="signup" element={<Signup />} />
-                <Route path="forgot" element={<Forgot />} />
-                {/* we want to protect these routes */}
-                <Route element={<PersistLogin />}>
-                  <Route
-                    element={
-                      <RequireAuth
-                        allowedRoles={[
-                          ROLES.User,
-                          ROLES.Super_Admin,
-                          ROLES.Approver,
-                          ROLES.Admin,
-                          ROLES.Author,
-                        ]}
-                      />
-                    }
-                  >
-                    <Route path="/*" element={<Dashboard />} />
+          <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  {/* public routes */}
+                  <Route path="/" element={<Home />} />
+                  <Route path="unathorized" element={<Unauthorized />} />
+                  <Route path="signin" element={<SignIn />} />
+                  <Route path="signup" element={<Signup />} />
+                  <Route path="forgot" element={<Forgot />} />
+                  {/* we want to protect these routes */}
+                  <Route element={<PersistLogin />}>
+                    <Route
+                      element={
+                        <RequireAuth
+                          allowedRoles={[
+                            ROLES.User,
+                            ROLES.Super_Admin,
+                            ROLES.Approver,
+                            ROLES.Admin,
+                            ROLES.Author,
+                          ]}
+                        />
+                      }
+                    >
+                      <Route path="/*" element={<Dashboard />} />
+                    </Route>
                   </Route>
-                </Route>
 
-                {/* catch all */}
-                <Route path="*" element={<Missing />} />
-              </Route>
-            </Routes>
-          </ThemeProvider>
+                  {/* catch all */}
+                  <Route path="*" element={<Missing />} />
+                </Route>
+              </Routes>
+            </ThemeProvider>
+          </ColorModeContext.Provider>
         </StyledEngineProvider>
       </IntlProvider>
     </LanguageProvider.Provider>
---
+++ src/context/ColorModeContext.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
+export default ColorModeContext;
